5814ab7e7d1e9ff686ca7412266b8d1dae20a634
kamilly <kamilly.medeiros@academico.ifpb.edu.br>
2025-09-23 06:46:24 -0300

    Initial commit

---
+++ src/test/java/execicio/Circulo.java
@@ -0,0 +1,34 @@
+package execicio;
+
+public class Circulo {
+    private float raio;
+    private final float PI = 3.14159f;
+
+    public Circulo(float raio) {
+        this.raio = raio;
+    }
+
+    public float getRaio() {
+        return this.raio;
+    }
+
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+
+    public float getArea() {
+        return PI * raio * raio; // área = pi * raio ao quadrado
+    }
+
+    public double getCircunferencia() {
+        return 2 * Math.PI * raio; // circunferência = 2 * pi * raio
+    }
+
+    // Método principal só para testar rodando direto
+    public static void main(String[] args) {
+        Circulo c = new Circulo(4.5f);
+        System.out.println("Raio: " + c.getRaio());
+        System.out.println("Área: " + c.getArea());
+        System.out.println("Circunferência: " + c.getCircunferencia());
+    }
+}
